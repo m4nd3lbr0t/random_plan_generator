af8edd0a7d9d60eb7ce9ad054e511b898e1c6bd7
Claudia <clau.rivera.gomez@gmail.com>
2020-04-04 11:52:33 +0200

    add some plans

---
+++ src/api/data.py
@@ -0,0 +1,22 @@
+plans = [
+    {
+        'id': 1,
+        'title': "Hacer una tortilla de patata"
+    },
+    {
+        'id': 2,
+        'title': "Jugar al LOL"
+    },
+    {
+        'id': 3,
+        'title': "Salir a escalar"
+    },
+    {
+        'id': 4,
+        'title': "Leer una obra de teatro"
+    },
+    {
+        'id': 5,
+        'title': "Escucha tu Daily Mix de Spotify"
+    },
+]
